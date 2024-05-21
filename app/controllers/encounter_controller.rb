282fae68e45b52965745bcee85e6605bddc9c4d5
Leon Skinner <Leon.skinner@bt.com>
2024-05-21 14:25:15 +0100

    generate model and controlers for users, games and encounters

---
+++ app/controllers/encounter_controller.rb
@@ -0,0 +1,10 @@
+class EncounterController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+end
---
+++ spec/controllers/encounter_controller_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe EncounterController, type: :controller do
+  describe "GET #index" do
+    it "returns all the encouters for a game" do
+      expect(true).to eq(true)
+    end
+  end
+end
