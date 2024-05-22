f3757b24f7b48a839c05294587b23c808791726f
Leon Skinner <Leon.skinner@bt.com>
2024-05-22 13:38:18 +0100

    add creaturs to encounter

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -2,7 +2,9 @@ class CreaturesController < ApplicationController
   def create
     @creature = Creature.new(create_params)
     @creature.encounter = Encounter.find(params[:encounter_id])
-    @creature.save
+    if @creature.save
+      redirect_to user_game_encounter_url(current_user, params[:game_id], params[:encounter_id])
+    end
   end
 
   private 
--- app/controllers/encounters_controller.rb
+++ app/controllers/encounters_controller.rb
@@ -1,16 +1,21 @@
 class EncountersController < ApplicationController
   def index
-    @encounter = current_user.games.find(params[:game_id]).encounters.all
+    @game = current_user.games.find(params[:game_id])
+    @encounter = @game.encounters.all
+    @new_encounter = Encounter.new
   end
 
   def show
     @encounter = current_user.games.find(params[:game_id]).encounters.find(params[:id])
+    @new_creature = Creature.new
   end
 
   def create
     @encounter = Encounter.new(create_params)
     @encounter.game = Game.find(params[:game_id])
-    @encounter.save
+    if @encounter.save
+      redirect_to user_game_encounters_url(current_user, params[:game_id])
+    end
   end
 
   private
