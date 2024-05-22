3904900f13f8e9766a950fae044a210de02f2537
Leon Skinner <Leon.skinner@bt.com>
2024-05-22 14:58:53 +0100

    fix issues of premade ac

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -12,8 +12,7 @@ class CreaturesController < ApplicationController
 
   def premade_create
     monster = MonsterService.fetch_monsters(params[:monster][:id])
-    debugger
-    @creature = Creature.new(name: monster["name"], health: monster["hit_points"], armor_class: monster["armor_class"].find { |ac| ac["type"] == "natural" }["value"], initiative: initiative)
+    @creature = Creature.new(name: monster["name"], health: monster["hit_points"], armor_class: monster["armor_class"].first["value"], initiative: initiative)
     @creature.encounter = Encounter.find(params[:encounter_id])
     if @creature.save
       redirect_to user_game_encounter_url(current_user, params[:game_id], params[:encounter_id])
